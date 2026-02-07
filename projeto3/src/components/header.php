ebc71990508d09fe1967da4dfed8d3c5b1d48c67
ClasseFinal <classefinal@classefinal.com.br>
2026-02-07 19:07:25 -0300

    wip: removed duplicated

--- projeto3/src/components/header.php
+++ projeto3/src/components/header.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * @psalm-import-type Route from types
  */
@@ -16,9 +18,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
-    <link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="./assets/fontawesome/css/all.min.css" rel="stylesheet">
-    <script src="./assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/fontawesome/css/all.min.css" rel="stylesheet">
+    <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <?php require_once 'navbar.php' ?>
