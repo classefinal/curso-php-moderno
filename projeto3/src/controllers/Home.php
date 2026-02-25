e45fa7102b58e90af1e301c06376a18e85216471
ClasseFinal <classefinal@classefinal.com.br>
2026-02-24 21:53:53 -0300

    wip: removed log defer

--- projeto3/src/controllers/Home.php
+++ projeto3/src/controllers/Home.php
@@ -15,17 +15,10 @@ declare(strict_types=1);
  */
 function makeHome(array $configs, array $route, ?string $uri): void
 {
-    $configs['defer'](function() {
-        sleep(10);
-        file_put_contents(BASE_PATH . DIRECTORY_SEPARATOR . "log.txt", "Terminou\n");
-    });
-
     makePage('home', [
         'title' => 'Página inicial',
         'routes' => getMenuItens($configs['routes'], $uri),
     ]);
-
-    setcookie('teste', '1');
 
     $configs['response'](200, '123');
 }
